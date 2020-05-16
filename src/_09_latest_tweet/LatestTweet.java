97cc302f2e863e6f066c7888f7901eee6a6c4d7a
angelvillarreal24 <angel.villarreal.24@bishops.com>
2020-05-16 11:28:40 -0700

    Finished... kind of LatestTweet
    le 1 token

--- src/_09_latest_tweet/LatestTweet.java
+++ src/_09_latest_tweet/LatestTweet.java
@@ -8,7 +8,7 @@ public class LatestTweet implements ActionListener {
 
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
-    JTextField text = new JTextField();
+    JTextField text = new JTextField(25);
     JButton button = new JButton();
 
     void createUI(){
@@ -16,12 +16,16 @@ public class LatestTweet implements ActionListener {
         panel.add(text);
         panel.add(button);
         frame.setVisible(true);
-        frame.setSize(500,500);
+        frame.setSize(500,100);
+        button.setText("Search the Twitterverse");
+        frame.setTitle("The Amazing Tweet Retriever");
+        text.setSize(100,20);
         button.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         System.out.println("tweet tweet");
+        JOptionPane.showMessageDialog(null, );
     }
 }
